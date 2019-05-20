5660d6cff99e95a23346020c46482ceef3300ae7
tang <506180102@qq.com>
2019-05-20 19:05:37 +0800

    第一次

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,5 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("输出信息");
+        System.out.println("老黑傻屌");
+        System.out.println("ddd");
     }
+
 }
